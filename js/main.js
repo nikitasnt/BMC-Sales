03cd8f1284bc012b1e2ffbfbdfdf92456256fa6e
Nikita <14nikita14m@gmail.com>
2020-04-25 17:14:06 +0400

    add section hero and adaptive

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  // свайпер секции hero
+  var mySwiperHero = new Swiper ('.hero__swiper-container', {
+
+  });
+  // переключения по этому слайдеру
+  $('.hero__slide-next').on('click', function() {
+    $('.hero__slide-next').removeClass('hero__slide_active');
+    $('.hero__slide-prev').addClass('hero__slide_active');
+    mySwiperHero.slideNext();
+  });
+  $('.hero__slide-prev').on('click', function() {
+    $('.hero__slide-prev').removeClass('hero__slide_active');
+    $('.hero__slide-next').addClass('hero__slide_active');
+    mySwiperHero.slidePrev();
+  });
+
+
+
+  // бургер
+  $('.header__burger').on('click', function() {
+    $('.header__burger').toggleClass('header__burger_active');
+    $('.nav').toggleClass('nav_active');
+  });
+});
