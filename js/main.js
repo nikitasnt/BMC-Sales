2e168bd0065f5cedd85092cd4d0595a1f5169efe
Nikita <14nikita14m@gmail.com>
2020-04-27 19:51:00 +0400

    add timer section and link on most-popular section

--- js/main.js
+++ js/main.js
@@ -30,4 +30,53 @@ $(document).ready(function () {
     $('.categories__hidden').slideToggle(600);
     $(this).toggleClass('categories__button_active');
   });
+
+
+
+  // таймер
+  function countdown(dateEnd) {
+    var timer, days, hours, minutes, seconds;
+
+    dateEnd = new Date(dateEnd);
+    dateEnd = dateEnd.getTime();
+
+    if (isNaN(dateEnd)) {
+      return;
+    }
+
+    timer = setInterval(calculate, 1000);
+
+    function calculate() {
+      var dateStart = new Date();
+      var dateStart = new Date(dateStart.getUTCFullYear(),
+        dateStart.getUTCMonth(),
+        dateStart.getUTCDate(),
+        dateStart.getUTCHours(),
+        dateStart.getUTCMinutes(),
+        dateStart.getUTCSeconds());
+      var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
+
+      if (timeRemaining >= 0) {
+        days = parseInt(timeRemaining / 86400);
+        timeRemaining = (timeRemaining % 86400);
+        hours = parseInt(timeRemaining / 3600);
+        timeRemaining = (timeRemaining % 3600);
+        minutes = parseInt(timeRemaining / 60);
+        timeRemaining = (timeRemaining % 60);
+        seconds = parseInt(timeRemaining);
+
+
+        document.getElementById("days").innerHTML = parseInt(days, 10);
+        document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
+        document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+        document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+      } else {
+        return;
+      }
+    }
+
+    function display(days, hours, minutes, seconds) {}
+  }
+
+  countdown('05/10/2020 02:00:00 AM');
 });
